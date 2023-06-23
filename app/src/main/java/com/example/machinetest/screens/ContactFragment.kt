b1505561b216d2d148e9a7d9b225bcd3a77a3468
joydip <joydipbose999@gmail.com>
2023-06-24 01:08:44 +0530

    COMMIT 6

---
+++ app/src/main/java/com/example/machinetest/screens/ContactFragment.kt
@@ -0,0 +1,30 @@
+package com.example.machinetest.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.machinetest.databinding.FragmentContactBinding
+
+
+class ContactFragment : Fragment() {
+    private var _binding : FragmentContactBinding?= null
+    private val binding : FragmentContactBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding =  FragmentContactBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tv.setOnClickListener {  }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/machinetest/screens/ProfileFragment.kt
@@ -0,0 +1,30 @@
+package com.example.machinetest.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.machinetest.databinding.FragmentProfileBinding
+
+
+class ProfileFragment : Fragment() {
+    private var _binding : FragmentProfileBinding?= null
+    private val binding : FragmentProfileBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding =  FragmentProfileBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tv.setOnClickListener {  }
+    }
+
+
+}
